c1a91598398b867b46ce1a647cf845bbf0ce57eb
Jdlxt <jdlxt@sd-160006.dedibox.fr>
2025-02-24 21:51:10 +0100

    misc fix

--- src/days.json.php
+++ src/days.json.php
@@ -1,13 +1,51 @@
 <?php
 
+// $data = [
+//     'Lundi' => rand(0, 100),
+//     'Mardi' => rand(0, 100),
+//     'Mercredi' => rand(0, 100),
+//     'Jeudi' => rand(0, 100),
+//     'Vendredi' => rand(0, 100),
+//     'Samedi' => rand(0, 100),
+//     'Dimanche' => rand(0, 100),
+// ];
+
 $data = [
-    'Lundi' => rand(0, 100),
-    'Mardi' => rand(0, 100),
-    'Mercredi' => rand(0, 100),
-    'Jeudi' => rand(0, 100),
-    'Vendredi' => rand(0, 100),
-    'Samedi' => rand(0, 100),
-    'Dimanche' => rand(0, 100),
+    [
+        'caption' => 'Sunday',
+        'day-index' => 0,
+        'value' => rand(0, 100),
+    ],
+    [
+        'caption' => 'Monday',
+        'day-index' => 1,
+        'value' => rand(0, 100),
+    ],
+    [
+        'caption' => 'Tuesday',
+        'day-index' => 2,
+        'value' => rand(0, 100),
+    ],
+    [
+        'caption' => 'Wednesday',
+        'day-index' => 3,
+        'value' => rand(0, 100),
+    ],
+    [
+        'caption' => 'Thursday',
+        'day-index' => 4,
+        'value' => rand(0, 100),
+    ],
+    [
+        'caption' => 'Friday',
+        'day-index' => 5,
+        'value' => rand(0, 100),
+    ],
+    [
+        'caption' => 'Saturday',
+        'day-index' => 6,
+        'value' => rand(0, 100),
+    ],
 ];
 
 header('Content-type: application/json');
--- src/index.php
+++ src/index.php
@@ -1,9 +1,48 @@
-<h1>Generators</h1>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Generators ; dummy data api</title>
+    <meta charset="utf-8">
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>⚙️</text></svg>">
+    <style>
+        html {
+            font-family: sans-serif;
+            background-color: #f0f0f0;
+        }
 
-<ul>
-    <li><a href="/persons.json.php">Persons</a></li>
-    <li><a href="/days.json.php">Days</a></li>
-    <li><a href="/scatter.json.php">Scatters</a></li>
-    <li><a href="/kaamelott/kaamelott.json.php?n=10">Kaamelott</a></li>
-    <li><a href="/french-address/city.php?max=7">Cities</a></li>
-</ul>
+    </style>
+</head>
+
+<body>
+    <h1>Generators</h1>
+    <p>
+        Some dummy data generators.
+    </p>
+    <p>
+        CORS are enabled, so you can use them in your front-end tests.
+    </p>
+    <div>
+        <h2>🔥 Disclaimer</h2>
+        <p>
+            Thoses endpoints must be used only for educational purpose.
+        </p>
+    </div>
+    <div>
+        <h2>📖 TODO</h2>
+        <p>
+            Each generator should have some documentation.
+        </p>
+    </div>
+
+    <section>
+        <h2>Generators</h2>
+        <ul>
+            <li><a href="/persons.json.php" target="_blank">Persons</a></li>
+            <li><a href="/days.json.php" target="_blank">Days with a random value</a></li>
+            <li><a href="/scatter.json.php" target="_blank">Persons on a scatter</a></li>
+            <li><a href="/kaamelott?n=10" target="_blank">Kaamelott</a></li>
+            <li><a href="/french-cities?max=7" target="_blank">Cities</a></li>
+        </ul>
+    </section>
+</body>
+</html>
